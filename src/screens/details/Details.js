b28db6c0a4d0c2c65709206580d7a1c7fd613283
Suhas M R <suhasmr96@gmail.com>
2021-07-26 21:54:34 +0530

    Added details page [contains details.js and details.css files

---
+++ src/screens/details/Details.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import { Router, Route, BrowserRouter, Switch, Link } from "react-router-dom";
+const Details = () => {
+  return <div></div>;
+};
+
+export default Details;
