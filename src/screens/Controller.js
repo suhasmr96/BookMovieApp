f2de492a2bc095585c6e33d0e03c7dc9f50e86f0
Suhas M R <suhasmr96@gmail.com>
2021-07-26 21:57:13 +0530

    Added controller file

---
+++ src/screens/Controller.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../common/header/Header";
+import Home from "../screens/home/Home";
+const Controller = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default Controller;
